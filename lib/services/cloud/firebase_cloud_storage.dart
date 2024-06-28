259c631d9f9216a21e74ff405186b7e3cec72c5c
iam-khd <khalidmuhammed31@gmail.com>
2024-06-28 09:56:23 +0530

    step-20

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -6,7 +6,7 @@ import 'package:mynotes/services/cloud/cloud_storage_exceptions.dart';
 class FireBaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
 
-  Future<void> delteNote({
+  Future<void> deleteNote({
     required String documentId,
   }) async {
     try {
@@ -42,23 +42,25 @@ class FireBaseCloudStorage {
             isEqualTo: ownerUserId,
           )
           .get()
-          .then((value) => value.docs.map((doc) {
-                return CloudNote(
-                  documentId: doc.id,
-                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
-                  text: doc.data()[textFieldName] as String,
-                );
-              }));
+          .then(
+            (value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)),
+          );
     } catch (e) {
       throw CouldNotGetAllNotesException;
     }
   }
 
-  void createNewNote({required String ownerUserId}) async {
-    await notes.add({
+  Future<CloudNote> createNewNote({required String ownerUserId}) async {
+    final document = await notes.add({
       ownerUserIdFieldName: ownerUserId,
       textFieldName: '',
     });
+    final fetchedNote = await document.get();
+    return CloudNote(
+      documentId: fetchedNote.id,
+      ownerUserId: ownerUserId,
+      text: '',
+    );
   }
 
   static final FireBaseCloudStorage _shared =
